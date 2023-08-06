9084b0504853b1994bdd19cb9f17511420938896
Abu <118349590+shadowfsdev@users.noreply.github.com>
2023-08-06 11:56:42 +1000

    Delete src/tailwindSucks directory

--- src/tailwindSucks/homeStyle.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-export const textDiv =
-  "absolute top-0 left-1/2 transform -translate-x-1/2 flex flex-row p-5 space-x-10";
-export const textLabel =
-  "text-white font-extrabold text-xsm uppercase text-shadow-lg whitespace-nowrap cursor-pointer hover:-translate-y-1 hover:text-black";
-export const textSelected =
-  "text-darkgray font-extrabold text-xsm uppercase text-shadow-lg whitespace-nowrap cursor-pointer hover:-translate-y-1 hover:text-black";
